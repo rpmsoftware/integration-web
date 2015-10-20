4b28dca975816b2a08146b668fba521f858b2db9
Dmitriy Serdyukov <dmitriy@rpmsoftware.com>
2015-10-20 10:47:11 -0600

    Added Office 365 Subscriptions

--- office365wh.js
+++ office365wh.js
@@ -1,44 +1,168 @@
-function Subscription(serviceRootUri, getAccessTokenFn) {
-    this._context = new Microsoft.OutlookServices.Extensions.DataContext(serviceRootUri, undefined, getAccessTokenFn);
+var uuid = require('node-uuid');
+var Microsoft = require("node-outlook").Microsoft;
+var outlook = Microsoft.OutlookServices;
+var Deferred = Microsoft.Utility.Deferred;
+
+var ODATA_TYPE_PUSH_SUBSCRIPTION = "#Microsoft.OutlookServices.PushSubscription";
+
+function Subscription(context, path, data) {
+	outlook.Entity.call(this, context, path, data);
+	this._odataType = ODATA_TYPE_PUSH_SUBSCRIPTION;
+
+	if (!data) {
+		return;
+	}
+
+	// "@odata.context": "https://outlook.office.com/api/beta/$metadata#Me/Subscriptions/$entity",
+	this._ResourceURL = data.ResourceURL;
+	this._ClientState = data.ClientState;
+	this._CallbackURL = data.CallbackURL;
+	this._ExpirationTime = new Date(data.ExpirationTime).getTime();
+	this._ChangeType = data.ChangeType;
+	this._AquiredTime = Date.now();
+	this._ttl = this._ExpirationTime - this._AquiredTime;
 }
 
-Object.defineProperty(Subscription.prototype, "context", {
-    get: function () {
-        return this._context;
-    },
-    enumerable: true,
-    configurable: true
+Subscription.prototype = Object.create(outlook.Entity.prototype);
+
+Object.defineProperty(Subscription.prototype, "resourceURL", {
+	get: function () {
+		return this._ResourceURL;
+	},
+	enumerable: true,
+	configurable: true
 });
 
-Subscription.prototype.getPath = function (prop) {
-    return this.context.serviceRootUri + '/' + prop;
+Object.defineProperty(Subscription.prototype, "clientState", {
+	get: function () {
+		return this._ClientState;
+	},
+	enumerable: true,
+	configurable: true
+});
+
+Object.defineProperty(Subscription.prototype, "callbackURL", {
+	get: function () {
+		return this._CallbackURL;
+	},
+	enumerable: true,
+	configurable: true
+});
+
+Object.defineProperty(Subscription.prototype, "changeType", {
+	get: function () {
+		return this._ChangeType;
+	},
+	enumerable: true,
+	configurable: true
+});
+
+
+Object.defineProperty(Subscription.prototype, "expirationTime", {
+	get: function () {
+		return new Date(this._ExpirationTime);
+	},
+	enumerable: true,
+	configurable: true
+});
+
+Object.defineProperty(Subscription.prototype, "aquiredTime", {
+	get: function () {
+		return new Date(this._AquiredTime);
+	},
+	enumerable: true,
+	configurable: true
+});
+
+Subscription.prototype.update = function () {
+	var _this = this;
+	var deferred = new Deferred();
+	var request = new outlook.Extensions.Request(this.getPath('renew'));
+
+	request.method = 'POST';
+
+	var expirationTime = Date.now() + this._ttl;
+
+	this.context.request(request).then(function (data) {
+		_this._AquiredTime = Date.now();
+		_this._ExpirationTime = expirationTime;
+		deferred.resolve(data);
+	}, deferred.reject.bind(deferred));
+	return deferred;
 };
 
-Object.defineProperty(Subscription.prototype, "users", {
-    get: function () {
-        if (this._Users === undefined) {
-            this._Users = new Users(this.context, this.getPath('Users'));
-        }
-        return this._Users;
-    },
-    enumerable: true,
-    configurable: true
-});
-
-/// <summary>
-/// There are no comments for Users in the schema.
-/// </summary>
-Subscription.prototype.addToUsers = function (user) {
-    this.users.addUser(user);
+Subscription.prototype.expired = function () {
+	return this._ExpirationTime < Date.now();
 };
 
-Object.defineProperty(Subscription.prototype, "me", {
-    get: function () {
-        if (this._Me === undefined) {
-            this._Me = new UserFetcher(this.context, this.getPath("Me"));
-        }
-        return this._Me;
-    },
-    enumerable: true,
-    configurable: true
+function Subscriptions(context, path) {
+	outlook.EntityFetcher.call(this, context, path);
+	this._Existing = [];
+}
+
+Subscriptions.prototype = Object.create(outlook.EntityFetcher.prototype);
+
+var normalizeChangeTypes = (function () {
+	var CHANGE_TYPES = { created: 'Created', updated: 'Updated', deleted: 'Deleted' };
+	var SEPARATOR = ',';
+	return function (changeTypes) {
+		if (typeof changeTypes === 'string') {
+			changeTypes = changeTypes.split(SEPARATOR);
+		}
+		if (!Array.isArray(changeTypes)) {
+			throw Error('Array or comma separated string is expected');
+		}
+		for (var ii = 0; ii < changeTypes.length; ii++) {
+			var typ = (changeTypes[ii] || '').trim();
+			var normalized = CHANGE_TYPES[typ.toLowerCase()];
+			if (!normalized) {
+				throw new Error('Unknown ChangeType: ' + typ);
+			}
+			changeTypes[ii] = normalized;
+		}
+		return changeTypes.join(SEPARATOR);
+	};
+})();
+
+Subscriptions.prototype.create = function (resource, callbackUrl, changeTypes, clientState) {
+	if (resource.context !== this.context) {
+		throw new Error('Wrong resource context');
+	}
+	var request = new outlook.Extensions.Request(this.path);
+	request.method = 'POST';
+	request.data = JSON.stringify({
+		'@odata.type': ODATA_TYPE_PUSH_SUBSCRIPTION,
+		ResourceURL: resource.path,
+		CallbackURL: callbackUrl,
+		ChangeType: normalizeChangeTypes(changeTypes),
+		ClientState: clientState || uuid.v4()
+	});
+	var _this = this;
+	var deferred = new Deferred();
+	this.context.request(request).then((function (data) {
+		data = JSON.parse(data);
+		data = new Subscription(_this.context, data['@odata.id'], data);
+		this._Existing.push(data);
+		deferred.resolve(data);
+	}).bind(this), deferred.reject.bind(deferred));
+	return deferred;
+};
+
+Object.defineProperty(outlook.UserFetcher.prototype, "subscriptions", {
+	get: function () {
+		if (this._Subscriptions === undefined) {
+			this._Subscriptions = new Subscriptions(this.context, this.getPath('Subscriptions'));
+		}
+		return this._Subscriptions;
+	},
+	enumerable: true,
+	configurable: true
 });
+
+(function () {
+	var parseEntityOriginal = outlook.Entity.parseEntity;
+	outlook.Entity.parseEntity = function (context, path, data) {
+		return (data && data['@odata.type'] === ODATA_TYPE_PUSH_SUBSCRIPTION) ? new Subscription(context, path, data) : parseEntityOriginal(context, path, data);
+	};
+})();
+
